2cabd13758d5334848215e3bd0331f1d5c5de61a
BivekChaudhary876 <bivekchaudhary798@gmail.com>
2024-05-01 17:42:45 +0545

    Day 23 task is updated

--- application/views/404_view.php
+++ application/views/404_view.php
@@ -1,7 +1,9 @@
-<div class="centered-container">
-    <div class="content">
-        <h1>404 - Controller Not Found</h1>
-        <p>Sorry, the page you were looking for does not exist. It might have been moved or deleted.</p>
-        <a href="<?= BASE_URL ?>" class="return-link">Return to Home</a>
+<div class="container" style="display:flex;justify-content:center;align-items:center; margin-top:140px; ">
+    <div class="content" style="display:flex; justify-content:center;align-items:center;flex-direction:column;">
+        <img class="mb-4 img-error" src="public/img/error-404-monochrome.svg" width="200" height="200">
+        <h1>Controller Not Found</h1>
+        <p style="text-align:center;">Sorry, the page you were looking for does not exist. It might have been moved or deleted.</p>
+        <a href="<?= BASE_URL ?>" class="return-link"><Return to Home</a>
     </div>
 </div>
+
--- application/views/dashboard_view.php
+++ application/views/dashboard_view.php
@@ -1,15 +1,14 @@
-
 <div class="my-3 text-center">
     <h1>Dashboard</h1>
 </div>
-<div class="row" id="droppable"style="gap:0px">
-    <div class="col-4" id="col1" style="border:1px solid green;">
+<div class="row py-4" id="droppable"style="gap:100px">
+    <div class="col-3" id="col1" style="border:1px solid green;">
         <p>widget 1</p>
     </div>
-    <div class="col-4" id="col2"  style="border:1px solid orange;">
+    <div class="col-3" id="col2"  style="border:1px solid orange;">
         <p>widget 2</p>
     </div>
-    <div class="col-4" id="col3"  style="border:1px solid blue;">
+    <div class="col-3" id="col3"  style="border:1px solid blue;">
         <p>widget 3</p>
     </div>
 </div>
@@ -44,67 +43,30 @@
     <?php endif; ?>
 </div>
 
-
-<?php 
-// Total leave List
-$data = [
-    "current_status" => ['pending','approved','rejected']
-];
-
-leave_list([
-    "modal-id"       => 'totalLeaveModal',  
-    "model"          => $total_leave_requests,
-    "current_status" => $data['current_status'][0]
-]);
-
-// Pending List
-leave_list([
-    "modal-id"       => 'pendingModal',
-    "model"          => $total_leave_requests,
-    "current_status" => 'pending'
-]);
-
-// Approved List
-leave_list([
-    "modal-id"       => 'approvedModal',
-    "model"          => $total_leave_requests,
-    "current_status" => 'approved'
-]);
-
-// Rejected List
-leave_list([
-    "modal-id"       => 'rejectedModal',
-    "model"          => $total_leave_requests,
-    "current_status" => 'rejected'
-]);?>
-
-
-
-
-<?php if( is_admin() ){?>
+<?php if( is_admin() ) :?>
   <div class="row">
       <div id="line" class="col-6">
 
           <?php
           $leaveCountsByMonth = [];
 
-          foreach ( $total_leave_requests as $leave_request ) {
-              $from = new DateTime( $leave_request['from_date'] );
+          foreach ( $total_leave_requests as $leave_request ) :
+              $from = new DateTime( $leave_request[ 'from_date' ] );
               $monthYear = $from->format('F Y');
 
-              if (isset($leaveCountsByMonth[$monthYear])) {
-                  $leaveCountsByMonth[$monthYear]++;
-              } else {
-                  $leaveCountsByMonth[$monthYear] = 1;
-              }
-          }
+              if ( isset( $leaveCountsByMonth[ $monthYear ] ) ) :
+                  $leaveCountsByMonth[ $monthYear ]++;
+               else :
+                  $leaveCountsByMonth[ $monthYear ] = 1;
+               endif;
+            endforeach;
           $months = [];
           $leaveCounts = [];
           
-          foreach ($leaveCountsByMonth as $month => $count) {
+          foreach ( $leaveCountsByMonth as $month => $count ) :
               $months[] = $month;
               $leaveCounts[] = $count;
-          }
+          endforeach;
           ?>
 
           <div class="my-3">
@@ -118,37 +80,37 @@ leave_list([
           $leaveTypes = [];
           $leaveCounters = [];
 
-          foreach ($total_leave_requests as $leave_request) {
-              $type = $leave_request['leave_type'];
-              if (!isset($leaveTypes[$type])) {
-                  $leaveTypes[$type] = 1;
-              } else {
-                  $leaveTypes[$type]++;
-              }
-          }
+          foreach ( $total_leave_requests as $leave_request ) :
+              $type = $leave_request[ 'leave_type' ];
+              if ( !isset( $leaveTypes[ $type ] ) ) :
+                  $leaveTypes[ $type ] = 1;
+               else :
+                  $leaveTypes[ $type ]++;
+              endif;
+            endforeach;
 
-          $leaveTypeLabels = array_keys($leaveTypes);
-          $leaveTypeCounts = array_values($leaveTypes);
+          $leaveTypeLabels = array_keys( $leaveTypes );
+          $leaveTypeCounts = array_values( $leaveTypes );
           ?>
       <div class="my-3">
-          <canvas id="leavePieChart" width="400" height="400"></canvas>
+          <canvas id="leavePieChart" width="200" height="200"></canvas>
       </div>
       </div>
   </div>
-  <?php }?>
-
+  <?php endif;?>
 
 <script>
   
     var ctx = document.getElementById('leaveChart').getContext('2d');
     var leaveChart = new Chart(ctx, {
-        type: 'line',
+        type: 'bar',
         data: {
             labels: <?= json_encode($months) ?>,
             datasets: [{
                 label: 'Leave Requests',
                 data: <?= json_encode($leaveCounts) ?>,
                 fill: false,
+                backgroundColor:'green',
                 borderColor: 'rgb(75, 192, 192)',
                 tension: 0.1
             }]
@@ -174,12 +136,12 @@ leave_list([
                 label: 'Leave Types',
                 data: leaveTypeCounts,
                 backgroundColor: [
-                    'rgba(255, 99, 132, 0.5)',
-                    'rgba(54, 162, 235, 0.5)',
-                    'rgba(255, 206, 86, 0.5)',
-                    'rgba(75, 192, 192, 0.5)',
-                    'rgba(153, 102, 255, 0.5)',
-                    'rgba(255, 159, 64, 0.5)'
+                    'rgba(255, 99, 132, 1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(153, 102, 255, 1)',
+                    'rgba(255, 159, 64, 1)'
                 ],
                 borderColor: [
                     'rgba(255, 99, 132, 1)',
@@ -202,91 +164,23 @@ leave_list([
         }
     });
     
-
-// $(document).ready(function () {
-//     // Make the items draggable
-//     $("#draggable-h, #totalLeaveBtn, #pendingBtn, #approvedBtn, #rejectedBtn").draggable({
-//         revert: "invalid",
-//         stack: ".draggable",
-//         zIndex: 1000,
-//     });
-
-//     // Set up sortable and droppable behavior for widgets
-//     $(".col-4").sortable({
-//         connectWith: ".col-4",
-//         placeholder: "sortable-placeholder",
-//         update: saveWidgetOrder,
-//     }).disableSelection();
-
-//     $(".col-4").droppable({
-//         accept: "#draggable-h, #totalLeaveBtn, #pendingBtn, #approvedBtn, #rejectedBtn",
-//         drop: function (event, ui) {
-//             var droppedItem = ui.draggable;
-//             $(this).append(droppedItem);
-//             saveWidgetOrder();
-//         },
-//     });
-
-//     // Function to save widget order to local storage
-//     function saveWidgetOrder() {
-//         let widgetOrder = {
-//             col1: $("#col1").sortable("toArray"),
-//             col2: $("#col2").sortable("toArray"),
-//             col3: $("#col3").sortable("toArray"),
-//         };
-
-//         localStorage.setItem("widgetOrder", JSON.stringify(widgetOrder));
-//     }
-
-//     // On page load, initialize the widgets based on stored order
-//     let savedWidgetOrder = localStorage.getItem("widgetOrder");
-//     if (savedWidgetOrder) {
-//         let order = JSON.parse(savedWidgetOrder);
-
-//         order.col1.forEach(function (widget) {
-//             $("#" + widget).appendTo("#col1");
-//         });
-//         order.col2.forEach(function (widget) {
-//             $("#" + widget).appendTo("#col2");
-//         });
-//         order.col3.forEach(function (widget) {
-//             $("#" + widget).appendTo("#col3");
-//         });
-//     }
-
-//     $("#droppable").droppable({
-//         accept: "#totalLeaveBtn",
-//         drop: function (event, ui) {
-//             alert("Dropped on Drop!");
-//             saveWidgetOrder();
-//         },
-//     });
-
-//     if (localStorage.getItem("dragDropped") === "true") {
-//         $("#draggable-h, #totalLeaveBtn, #pendingBtn, #approvedBtn, #rejectedBtn").appendTo("#droppable").css({
-//             top: 0,
-//             left: 0,
-//         });
-//         alert("Drag was previously dropped and re-initialized.");
-//     }
-// });
-
 $(document).ready(function () {
     // Make the items draggable
     $("#draggable-h, #totalLeaveBtn, #pendingBtn, #approvedBtn, #rejectedBtn").draggable({
+        containment: 'document',
         revert: "invalid",
         stack: ".draggable",
         zIndex: 1000,
     });
 
     // Set up sortable and droppable behavior for widgets
-    $(".col-4").sortable({
-        connectWith: ".col-4",
+    $(".col-3").sortable({
+        connectWith: ".col-3",
         placeholder: "sortable-placeholder",
         update: saveWidgetOrder,
     }).disableSelection();
 
-    $(".col-4").droppable({
+    $(".col-3").droppable({
         accept: "#draggable-h, #totalLeaveBtn, #pendingBtn, #approvedBtn, #rejectedBtn",
         drop: function (event, ui) {
             var droppedItem = ui.draggable;
@@ -338,7 +232,7 @@ $(document).ready(function () {
 
     // Additional code for dragging out from the droppable container
     $("#droppable").sortable({
-        connectWith: ".col-4",
+        connectWith: ".col-3",
         placeholder: "sortable-placeholder",
         update: saveWidgetOrder,
     }).disableSelection();
@@ -350,6 +244,11 @@ $(document).ready(function () {
         });
         alert("Drag was previously dropped and re-initialized.");
     }
+    $("#draggable-h, #totalLeaveBtn, #pendingBtn, #approvedBtn, #rejectedBtn").draggable({
+        revert: function (droppable) {
+            return !droppable;
+        }
+    });
 });
 
 
